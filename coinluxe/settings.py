f583873fa48fbd82d88df5ebd6a12d7b2a744125
cafalchio <maolivei@tcd.ie>
2023-05-17 23:46:17 +0100

    changing settings for deployement

--- coinluxe/settings.py
+++ coinluxe/settings.py
@@ -163,10 +163,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR,'theme', 'static')
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'theme', 'static'),)
-MEDIA_URL = os.path.join(BASE_DIR,'media'),
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
@@ -192,3 +192,6 @@ if 'USE_AWS' in os.environ:
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+print(BASE_DIR)
