89f89db0a3460cc7ed30d70d2b0b14f0a9f11cb5
cafalchio <maolivei@tcd.ie>
2023-05-17 23:36:56 +0100

    vhange settings / debugging deploy

--- coinluxe/settings.py
+++ coinluxe/settings.py
@@ -163,10 +163,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/theme/static/'
+STATIC_URL = os.path.join(BASE_DIR,'theme', 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'theme', 'static'),)
-MEDIA_URL = '/media/'
+MEDIA_URL = os.path.join(BASE_DIR,'media'),
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
