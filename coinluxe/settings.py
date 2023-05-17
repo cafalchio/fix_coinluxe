2d287307569be645f6d4a1c281a6f725494c584d
cafalchio <maolivei@tcd.ie>
2023-05-17 23:24:39 +0100

    debuging deployement

--- coinluxe/settings.py
+++ coinluxe/settings.py
@@ -177,9 +177,9 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     
     # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATIC_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MEdiaStorage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+    STATIC_LOCATION = 'theme/static'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     MEDIAFILES_LOCATION = 'media'
     
     # Override static and media URLs in production
