3de6f728d0aaa482867fb8875edec4b28638295f
cafalchio <maolivei@tcd.ie>
2023-05-18 22:27:31 +0100

    updated settings

--- coinluxe/settings.py
+++ coinluxe/settings.py
@@ -165,10 +165,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'theme', 'static'),
+]
+
 
 STATIC_URL = '/theme/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 if 'USE_AWS' in os.environ:
