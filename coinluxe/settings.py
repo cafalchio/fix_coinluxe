0e2b3fe3fa3fb47c8e2a498dbbd472551da14b1d
cafalchio <maolivei@tcd.ie>
2023-05-18 09:56:27 +0100

    debugging deployement changes

--- coinluxe/settings.py
+++ coinluxe/settings.py
@@ -180,7 +180,7 @@ if 'USE_AWS' in os.environ:
     
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
+    STATICFILES_LOCATION = 'theme/static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
     
