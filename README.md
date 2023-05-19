6665a77b6b1511ebf63beca1cbb4f46f8bb116e1
cafalchio <maolivei@tcd.ie>
2023-05-19 13:56:07 +0100

    added  media images

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CoinLuxe
 Embrace the future of finance with CoinLuxe. Start your crypto journey today and unlock the potential of decentralized technologies.
+
+https://picsvg.com/ 
+https://www.youtube.com/watch?v=K1e8kpoag0E  
+https://www.youtube.com/watch?v=miiPsBlqMns
+https://tailwindcss.com/
+https://www.khanna.law/blog/deploying-django-tailwind-to-heroku
+https://looka.com
--- coinluxe/urls.py
+++ coinluxe/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from coinluxe import settings
 from home.views import index 
 from theme.views import change_theme
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +12,4 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('switch-theme/' , change_theme, name='change_theme'),
     path("", index, name="home")
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
