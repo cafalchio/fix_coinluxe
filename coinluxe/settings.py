a64d480c3d9251d12d0f9e8308e900a797689a20
cafalchio <maolivei@tcd.ie>
2023-05-20 13:39:06 +0100

    remove security warnings

--- coinluxe/settings.py
+++ coinluxe/settings.py
@@ -171,9 +171,9 @@ STATICFILES_DIRS = [
 
 
 # STATIC_URL = '/theme/static/'
-# MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 if 'USE_AWS' in os.environ:
@@ -198,3 +198,9 @@ if 'USE_AWS' in os.environ:
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SECURE_HSTS_SECONDS = 31536000*2
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
