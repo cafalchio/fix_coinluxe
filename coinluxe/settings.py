cb7c32c4c79362077b4b573a8e8f35c29318d73b
cafalchio <maolivei@tcd.ie>
2023-05-25 11:49:48 +0100

    included tailwind in settings

--- coinluxe/settings.py
+++ coinluxe/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     # tailwind
-    # 'tailwind',
+    'tailwind',
 
     # 'django_browser_reload',
     # allauth
