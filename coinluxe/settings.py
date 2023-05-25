83ecb56ff87cab462ce64a517fc1d618e814010b
cafalchio <maolivei@tcd.ie>
2023-05-25 09:20:35 +0100

    removed django reload for testing

--- coinluxe/settings.py
+++ coinluxe/settings.py
@@ -113,23 +113,11 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'coinluxe.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 DATABASES = {
     'default': dj_database_url.parse(DATABASE_URL)
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -148,7 +136,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
