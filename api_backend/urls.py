74f12771d3bf5ed9f3181db6d2f8e8c4c68cf888
cafalchio <maolivei@tcd.ie>
2023-06-02 20:40:10 +0100

    fixed url for cryptos

--- api_backend/urls.py
+++ api_backend/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CryptoListView
 
 urlpatterns = [
-    path("cryptos/", CryptoListView.as_view(), name="cryptos"),
+    path("", CryptoListView.as_view(), name="cryptos"),
     # Other URL patterns
 ]
