ac211fc6b4cec6efac2269352d66de12be9452c6
cafalchio <maolivei@tcd.ie>
2023-06-02 20:44:27 +0100

    up dated navbar link

--- api_backend/urls.py
+++ api_backend/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CryptoListView
 
 urlpatterns = [
-    path("", CryptoListView.as_view(), name="cryptos"),
+    path("list/", CryptoListView.as_view(), name="cryptos"),
     # Other URL patterns
 ]
