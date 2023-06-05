1682ac0d478359fdb4e5fc05ce9aee0eba30853a
cafalchio <maolivei@tcd.ie>
2023-06-05 15:21:08 +0100

    added commented models

--- portifolio/models.py
+++ portifolio/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+# from django.db import models
+# from django.contrib.auth.models import User
+# from api_backend.models import CryptoCurrency
 
-# Create your models here.
+
+# class Holding(models.Model):
+#     portfolio = models.ForeignKey('Portfolio', on_delete=models.CASCADE)
+#     cryptocurrency = models.ForeignKey(CryptoCurrency, on_delete=models.CASCADE)
+#     amount = models.FloatField(null=True)
+
+
+# class Portfolio(models.Model):
+#     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+#     holdings = models.ManyToManyField(CryptoCurrency, through=Holding)
