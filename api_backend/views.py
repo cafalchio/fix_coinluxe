b3465f689c56a08dde911cd5f2f362b79fe74e2a
cafalchio <maolivei@tcd.ie>
2023-06-09 18:44:46 +0100

    updated cryptos view

--- api_backend/views.py
+++ api_backend/views.py
@@ -1,21 +1,10 @@
 from django.views.generic import ListView
 from .models import CryptoCurrency
 
-
 class CryptoListView(ListView):
+    paginate_by = 25
     model = CryptoCurrency
     template_name = "api_backend/cryptos.html"
-    # context_object_name = "cryptos"
-    paginate_by = 10  # Number of items to display per page
-
+    
     def get_queryset(self):
-        queryset = super().get_queryset()
-
-        # Sort by price if the 'sort' parameter is 'price', otherwise sort by market_cap
-        sort_param = self.request.GET.get('sort')
-        if sort_param == 'price':
-            queryset = queryset.order_by('price')
-        else:
-            queryset = queryset.order_by('market_cap')
-
-        return queryset
+        return CryptoCurrency.objects.order_by('-market_cap')
