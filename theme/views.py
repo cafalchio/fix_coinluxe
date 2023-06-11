84fe7a6867a1e6f4bff3c94b8f2a675a9be6b9f2
cafalchio <maolivei@tcd.ie>
2023-06-11 12:54:04 +0100

    added footer

--- theme/views.py
+++ theme/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import redirect
 from django.http import HttpResponseRedirect
 
-def change_theme(request, **kwargs):
+def change_theme(request, *args, **kwargs):
     if "is_dark_theme" in request.session:
         request.session["is_dark_theme"] = not request.session.get("is_dark_theme")
     else:
