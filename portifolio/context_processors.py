196a8fd367cd3d3b422c9cbdbec87fbdd33f02a5
cafalchio <maolivei@tcd.ie>
2023-06-11 21:10:58 +0100

    fixed credits

--- portifolio/context_processors.py
+++ portifolio/context_processors.py
@@ -2,9 +2,11 @@ from portifolio.models import Credits
 
 def credits(request):
     if request.user.is_authenticated:
-        credits = Credits.objects.filter(user=request.user).first()
-        if credits is None:
+        credits_obj = Credits.objects.filter(user=request.user).first()
+        if credits_obj is None:
             credits = "0.00"
+        else:
+            credits = credits_obj.amount 
         return {'credits': credits}
     else:
         return {'credits': "0.00"}
