3bfdf4341a8eb2935255192387e1c72efd977f0e
cafalchio <maolivei@tcd.ie>
2023-06-13 11:52:44 +0100

    correct links

--- portifolio/urls.py
+++ portifolio/urls.py
@@ -3,8 +3,8 @@ from .views import add_credits, get_credits, payment_cancelled, payment_successf
 
 urlpatterns = [
     path("get-credits/", get_credits, name="get_credits"),
-    path("add_credits", add_credits, name="add_credits"),
-    path("payment_successful", payment_successful, name="payment_successful"),
-    path("payment_cancelled", payment_cancelled, name="payment_cancelled"),
-    path("stripe_webhook", stripe_webhook, name="stripe_webhook"),
+    path("add_credits/", add_credits, name="add_credits"),
+    path("payment_successful/", payment_successful, name="payment_successful"),
+    path("payment_cancelled/", payment_cancelled, name="payment_cancelled"),
+    path("stripe_webhook/", stripe_webhook, name="stripe_webhook"),
 ]
