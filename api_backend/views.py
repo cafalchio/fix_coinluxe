22f7733819f06dbebeea87d8270f60ebdc818a71
cafalchio <maolivei@tcd.ie>
2023-06-14 13:33:34 +0100

    improved click and search

--- api_backend/views.py
+++ api_backend/views.py
@@ -1,15 +1,24 @@
 from typing import Any, Dict
 from django.views.generic import ListView, DetailView
 from .models import Coins, CryptoCurrency
+from django.db.models import Q
 
 
 class CryptoListView(ListView):
-    paginate_by = 25
+    paginate_by = 15
     model = CryptoCurrency
     template_name = "api_backend/cryptos.html"
     
     def get_queryset(self):
-        return CryptoCurrency.objects.order_by('-market_cap')
+        queryset =  CryptoCurrency.objects.order_by('-market_cap')
+        search_query = self.request.GET.get('search')
+        if search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=search_query) |
+                Q(symbol__icontains=search_query)
+            )
+
+        return queryset
     
 class CoinDetailView(DetailView):
     model = Coins
