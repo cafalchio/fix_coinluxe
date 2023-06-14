ad90b4c6641b3d6fe3b23b65a13c79b13c18c4df
cafalchio <maolivei@tcd.ie>
2023-06-14 02:04:25 +0100

    return the old home view

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
-from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="/home/index.html"), name='home')
+    path('', views.index, name='home')
 ]
