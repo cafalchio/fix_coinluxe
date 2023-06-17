2ac082190260ee96a30d75cfcd980f2a60dc58e8
cafalchio <maolivei@tcd.ie>
2023-06-17 15:26:05 +0100

    added sell crypto

--- portifolio/forms.py
+++ portifolio/forms.py
@@ -7,3 +7,7 @@ class AddCreditsForm(forms.Form):
 class BuyCryptoForm(forms.Form):
     crypto_id = forms.CharField(widget=forms.HiddenInput())
     amount = forms.DecimalField(label='Amount')
+    
+class SellCryptoForm(forms.Form):
+    crypto_id = forms.CharField(widget=forms.HiddenInput())
+    amount = forms.DecimalField(label='Amount')
