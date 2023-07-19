0b2ef3f4a6b37ac818fedcfcc82a55790ee3e3a6
cafalchio <maolivei@tcd.ie>
2023-07-20 00:38:55 +0200

    return url

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from .views import IndexListView, test_view
 
 urlpatterns = [
     path("", IndexListView.as_view(), name="home"),
-    # path("loaderio-f7cb4845fcb26f88ba745418682e372e.txt", test_view, name="test-view"),
+    path("loaderio-f7cb4845fcb26f88ba745418682e372e.txt", test_view, name="test-view"),
 ]
 
